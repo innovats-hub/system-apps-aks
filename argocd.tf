dcc2f086b9e21ce64f6b581be0e4db7ee3c17b75
doouglasnasc <dodo_avaiano_1998@outlook.com>
2024-10-25 21:33:14 -0300

    feat: Deploy Jaeger app in ArgoCD

--- argocd.tf
+++ argocd.tf
@@ -33,7 +33,30 @@ resource "helm_release" "argocd" {
   depends_on = [kubernetes_namespace.namespace-argocd]
 }
 
-# Deploy Argocd in cluster
+# Deploy Jaeger in cluster
+resource "helm_release" "argocd_app_jaeger" {
+  count            = var.argocd_apps_enabled == true ? 1 : 0
+  name             = "${var.argocd_apps_resource_name}-jaeger"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-apps"
+  version          = var.argocd_apps_version
+  namespace        = kubernetes_namespace.namespace-argocd.metadata[0].name
+  force_update     = var.force_update
+  wait             = var.wait
+  reuse_values     = var.reuse_values
+  replace          = var.replace
+  timeout          = var.timeout
+  disable_webhooks = var.disable_webhooks
+  recreate_pods    = var.recreate_pods
+
+  values = [
+    "${file("values/app-jaeger-argo.yml")}"
+  ]
+
+  depends_on = [helm_release.argocd]
+}
+
+# Deploy Kiali in cluster
 resource "helm_release" "argocd_app_kiali" {
   count            = var.argocd_apps_enabled == true ? 1 : 0
   name             = "${var.argocd_apps_resource_name}-kiali"
@@ -53,5 +76,5 @@ resource "helm_release" "argocd_app_kiali" {
     "${file("values/app-kiali-argo.yml")}"
   ]
 
-  depends_on = [helm_release.argocd]
+  depends_on = [helm_release.argocd, helm_release.argocd_app_jaeger]
 }
