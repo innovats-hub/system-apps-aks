c87fb605236e2efb5f9df98d2a2cf20971102b0e
doouglasnasc <dodo_avaiano_1998@outlook.com>
2024-11-02 17:14:10 -0300

    feat: Install Harbor via ArgoCD

--- argocd.tf
+++ argocd.tf
@@ -102,6 +102,29 @@ resource "helm_release" "argocd_app_grafana" {
   depends_on = [helm_release.argocd]
 }
 
+# Deploy repositories apps in cluster
+resource "helm_release" "argocd_app_repositories" {
+  count            = var.argocd_apps_enabled == true ? 1 : 0
+  name             = "${var.argocd_apps_resource_name}-repositories"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-apps"
+  version          = var.argocd_apps_version
+  namespace        = helm_release.argocd[0].namespace
+  force_update     = var.force_update
+  wait             = var.wait
+  reuse_values     = var.reuse_values
+  replace          = var.replace
+  timeout          = var.timeout
+  disable_webhooks = var.disable_webhooks
+  recreate_pods    = var.recreate_pods
+
+  values = [
+    "${file("values/app-repositories-argo.yml")}"
+  ]
+
+  depends_on = [helm_release.argocd, kubectl_manifest.gateway_ingress_dev]
+}
+
 # Deploy DNS apps in cluster
 resource "helm_release" "argocd_app_dns" {
   count            = var.argocd_apps_enabled == true && var.letsencrypt_cloudflare_enabled == true ? 1 : 0
